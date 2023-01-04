170cd05e927e3f5df4c6751a611bbf08a88a7154
ricardoboolean <65415577+ricardoboolean@users.noreply.github.com>
2023-01-04 17:04:02 +0000

    Update test.py

--- dags/test-kafka/test.py
+++ dags/test-kafka/test.py
@@ -1,3 +1,5 @@
+# kafka_example_dag_1.py 
+
 import os
 import json
 import logging
@@ -15,3 +17,26 @@ connection_config = {
     "bootstrap.servers": os.environ["BOOSTRAP_SERVER"],
     "security.protocol": os.environ["SECURITY_PROTOCOL"],
     "sasl.mechanism": "PLAIN",
+    "sasl.username": os.environ["KAFKA_API_KEY"],
+    "sasl.password": os.environ["KAFKA_API_SECRET"]
+}
+
+with DAG(
+    dag_id="kafka_example_dag_1",
+    start_date=datetime(2022, 11, 1),
+    schedule=None,
+    catchup=False,
+):
+
+    # define the producer function
+    def producer_function():
+        for i in range(5):
+            yield (json.dumps(i), json.dumps(i+1))
+
+    # define the producer task
+    producer_task = ProduceToTopicOperator(
+        task_id=f"produce_to_{my_topic}",
+        topic=my_topic,
+        producer_function=producer_function, 
+        kafka_config=connection_config
+    )
