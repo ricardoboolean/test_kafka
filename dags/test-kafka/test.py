f11563d9eb74ba7d4e97ee75a2bd66488d3c26ef
ricardoboolean <65415577+ricardoboolean@users.noreply.github.com>
2023-01-04 17:22:16 +0000

    Update test.py

--- dags/test-kafka/test.py
+++ dags/test-kafka/test.py
@@ -29,9 +29,9 @@ with DAG(
 ):
 
     # define the producer function
-def producer_function():
-    for i in range(5):
-        yield (json.dumps(i), json.dumps(i+1))
+    def producer_function():
+        for i in range(5):
+            yield (json.dumps(i), json.dumps(i+1))
 
     # define the producer task
     producer_task = ProduceToTopicOperator(
